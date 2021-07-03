a835b2ccca4bfb2fdd54451d9bfe2251bfc4eaf7
rlatn <rlatn@DESKTOP-KMHIAH6>
2021-07-03 09:48:09 +0900

    chap4

---
+++ chapter4/src/pk4/Arith_Test_02.java
@@ -0,0 +1,16 @@
+package pk4;
+
+public class Arith_Test_02 {
+
+	public static void main(String[] args) {
+		int mathScore= 90;
+		int engScore= 70;
+		int totalScore=mathScore+engScore;
+		System.out.println(totalScore);
+		
+		int average = (totalScore)/2;
+		System.out.println(average);
+
+	}
+
+}
