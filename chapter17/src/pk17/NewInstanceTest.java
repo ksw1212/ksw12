013d3b7ac4cc4ac193f008ae9fe41d688dd6139c
rlatn <rlatn@DESKTOP-KMHIAH6>
2021-07-03 09:45:58 +0900

    chap17

--- chapter17/src/pk17/NewInstanceTest.java
+++ chapter17/src/pk17/NewInstanceTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package pk17;
 
 public class NewInstanceTest {
@@ -17,23 +17,3 @@ public class NewInstanceTest {
 	}
 
 }
-=======
-package pk17;
-
-public class NewInstanceTest {
-
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-		Person person1 = new Person();
-		System.out.println(person1);
-		
-		Class pClass = Class.forName("pk17.Person");
-		System.out.println(pClass);
-		//곧 없어질 메서드
-		Person person2=(Person)pClass.newInstance();
-		System.out.println(person2);
-		
-		String str = "";
-	}
-
-}
->>>>>>> refs/remotes/origin/master
